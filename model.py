3d2f69c102d437fc24c255b25b824ef6a7c348a8
dappon4 <dappon.04@gmail.com>
2024-10-26 04:26:23 -0400

    finally finished training

--- model.py
+++ model.py
@@ -14,135 +14,245 @@ import matplotlib.pyplot as plt
 from datamodule import MNISTDataModule
 
 class Generator(nn.Module):
-    def __init__(self, latent_dim=100):
+    def __init__(self, latent_dim, embedding_dim):
         super().__init__()
-
-        self.latent_dim = latent_dim
+        
+        self.z_proj = nn.Linear(latent_dim, 4*4*256)
+        self.embedded_proj = nn.Linear(embedding_dim, 4*4*256)
+        
+        self.tc1 = nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False)
+        self.tc2 = nn.ConvTranspose2d(256, 128, 4, 2, 2, bias=False)
+        self.tc3 = nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False)
+        
+        self.bn1 = nn.BatchNorm2d(256)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(64)
+        
+        self.conv2d = nn.Conv2d(64, 1, 1, 1, bias=False)
         
         self.leaky_relu = nn.LeakyReLU(0.2)
         
-        self.lienar1 = nn.Linear(latent_dim, 256)
-        self.linear2 = nn.Linear(256, 7*7*16)
+    def forward(self, z, embedded_labels):
         
-        self.tc1 = nn.ConvTranspose2d(16, 64, 4, 2, 1)
-        self.tc2 = nn.ConvTranspose2d(64, 1, 4, 2, 1)
+        batch_size = z.shape[0]
         
-    def forward(self, z):
-        z = self.leaky_relu(self.lienar1(z))
-        z = self.leaky_relu(self.linear2(z))
+        z = self.z_proj(z).reshape(batch_size, 256, 4, 4)
+        embedded_labels = self.embedded_proj(embedded_labels).reshape(batch_size, 256, 4, 4)
         
-        z = z.reshape(z.shape[0], 16, 7, 7)
+        z = torch.cat((z, embedded_labels), dim=1) # batch, 512, 4, 4
         
-        z = self.tc1(z) # batch, 64, 14, 14
+        z = self.tc1(z) # batch, 256, 8, 8
+        z = self.bn1(z)
         z = self.leaky_relu(z)
-        z = self.tc2(z)
-        z = torch.tanh(z)
 
+        z = self.tc2(z) # batch, 128, 14, 14
+        z = self.bn2(z)
+        z = self.leaky_relu(z)
+
+        z = self.tc3(z) # batch, 64, 28, 28
+        z = self.bn3(z)
+        z = self.leaky_relu(z)
+
+        z = self.conv2d(z)
+        z = self.leaky_relu(z)
+        z = torch.tanh(z)
+        
         return z
 
-class Disciminator(nn.Module):
-    def __init__(self, img_shape):
+class Discriminator(nn.Module):
+    def __init__(self, img_shape, embedding_dim):
         super().__init__()
+        
+        conv_dim_1 = 16
+        conv_dim_2 = 64
+        
+        self.leaky_relu = nn.LeakyReLU(0.2)
+        self.dropout = nn.Dropout(0.3)
+        self.sigmoid = nn.Sigmoid()
+        
+        self.conv1 = nn.Conv2d(1+1, conv_dim_1, 4, 2, 1)
+        self.conv2 = nn.Conv2d(conv_dim_1, conv_dim_2, 4, 2, 1)
+        
+        self.linear1 = nn.Linear(conv_dim_2*7*7, 128)
+        self.linear2 = nn.Linear(128, 1)
+        
+        self.embedded_linear = nn.Linear(embedding_dim, np.prod(img_shape[1:]))
+        
+    def forward(self, img, embedded_labels):
+        
+        batch_size = img.shape[0]
+        
+        embedded_labels = self.embedded_linear(embedded_labels).reshape(batch_size, 1, 28, 28)
+        
+        img = self.leaky_relu(img)
+        img = torch.cat((img, embedded_labels), dim=1) # batch, 2, 28, 28
 
+        img = self.conv1(img) # batch, 64, 14, 14
+        img = self.leaky_relu(img)
+        img = self.dropout(img)
+        
+        img = self.conv2(img) # batch, 128, 7, 7
+        img = self.leaky_relu(img)
+        img = self.dropout(img)
+        
+        img = img.view(batch_size, -1)
+        img = self.leaky_relu(self.linear1(img))
+        img = self.linear2(img)
+        
+        #not needed for wasserstein loss
+        img = self.sigmoid(img)
+
+        return img
+
+class Discriminator_2(nn.Module):
+    def __init__(self, img_shape, embedding_dim):
+        super().__init__()
+        
+        self.embedded_linear = nn.Linear(embedding_dim, np.prod(img_shape[1:]))
+        
         self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.Conv2d(1+1, 1, 1, 1),
+            nn.Flatten(),
             nn.LeakyReLU(0.2),
-            nn.Linear(512, 256),
+            nn.Linear(28*28, 1024),
             nn.LeakyReLU(0.2),
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 128),
+            nn.LeakyReLU(0.2),
+            nn.Linear(128, 1),
+            nn.Sigmoid()
         )
-
-    def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        validity = self.model(img_flat)
-
-        return validity
+    
+    def forward(self, img, embedded_labels):
+        batch_size = img.shape[0]
+        
+        embedded_labels = self.embedded_linear(embedded_labels).reshape(batch_size, 1, 28, 28)
+        
+        img = torch.cat((img, embedded_labels), dim=1)
+        
+        return self.model(img)
 
 class EmbeddedConditionlGAN(pl.LightningModule):
-    def __init__(self, img_shape, lr, b1, b2, latent_dim=100):
+    def __init__(self, img_shape, g_lr, d_lr, b1, b2, n_critics=5, embedding_dim=100, latent_dim=100, label_smoothing_factor=0.1, gradient_clipping_value=0.5):
         super().__init__()
         self.save_hyperparameters()
         
         self.img_shape = img_shape
-        self.lr = lr
+        self.g_lr = g_lr
+        self.d_lr = d_lr
         self.b1 = b1
         self.b2 = b2
+        self.n_critics = n_critics
+        self.label_smoothing_factor = label_smoothing_factor
+        self.gradient_clipping_value = gradient_clipping_value
         
-        self.generator = Generator()
-        self.discriminator = Disciminator(img_shape=img_shape)
+        self.generator = Generator(latent_dim, embedding_dim)
+        #self.discriminator = Discriminator(img_shape, embedding_dim)
+        self.discriminator = Discriminator_2(img_shape, embedding_dim)
         
-        self.emb = nn.Embedding(10, latent_dim)
-        self.d_emb_linear = nn.Linear(latent_dim, np.prod(img_shape))
+        self.emb = nn.Embedding(10, embedding_dim)
         
         self.leaky_relu = nn.LeakyReLU(0.2)
 
         # for validation
-        self.validation_z = torch.randn(10, latent_dim)
-        self.validation_labels = torch.arange(0, 10)
+        self.latent_dim = latent_dim
         
         # model config
         self.automatic_optimization = False
         
-    def forward(self, z):
-        return self.generator(z)
+    def forward(self, z, embedded_labels):
+        return self.generator(z, embedded_labels)
 
     def adversarial_loss(self, y_hat, y):
         return F.binary_cross_entropy(y_hat, y)
     
+    def wasserstein_loss(self, y_hat, y):
+        return -torch.mean(y_hat * y)
+    
     def training_step(self, batch):
         imgs, labels = batch
+        
+        ################
+        # checking the data
+        #print(labels)
+        #grid = torchvision.utils.make_grid(imgs, nrow=5)
+        #plt.figure(figsize=(10, 10))
+        #plt.imshow(grid.permute(1, 2, 0).cpu().numpy(), cmap='gray')
+        #plt.axis('off')
+        #plt.show()
+        ################
         
         optimizer_g, optimizer_d = self.optimizers()
         
         batch_size = imgs.shape[0]
-        z = torch.randn(batch_size, self.hparams.latent_dim).to(self.device)
-        z.type_as(imgs)
+        
 
         embedded_labels = self.emb(labels)
-        discriminator_embedded_labels = self.d_emb_linear(embedded_labels).reshape(batch_size, *self.img_shape)
         
         # train generator
         self.toggle_optimizer(optimizer_g)
-        self.generated_imgs = self(z + embedded_labels)
+    
+        z = torch.randn(batch_size, self.hparams.latent_dim).to(self.device)
         
-        valid = torch.ones(imgs.size(0), 1)
+        self.generated_imgs = self(z, embedded_labels)
+        
+        valid = torch.ones(imgs.size(0), 1) - self.hparams.label_smoothing_factor
         valid = valid.type_as(imgs)
         
-        g_loss = self.adversarial_loss(self.discriminator(self.generated_imgs + discriminator_embedded_labels), valid)
+        g_loss = self.adversarial_loss(self.discriminator(self.generated_imgs, embedded_labels), valid)
         
         self.manual_backward(g_loss, retain_graph=True)
+        #self.manual_backward(g_loss)
+        if self.hparams.gradient_clipping_value > 0:
+            self.clip_gradients(optimizer_g, gradient_clip_val=self.gradient_clipping_value, gradient_clip_algorithm='norm')
         optimizer_g.step()
         optimizer_g.zero_grad()
+        
+        
         self.untoggle_optimizer(optimizer_g)
         
         # train discriminator
-        self.toggle_optimizer(optimizer_d)
+        #self.toggle_optimizer(optimizer_d)
         
-        valid = torch.ones(imgs.size(0), 1)
-        valid = valid.type_as(imgs)
+        for _ in range(self.n_critics):
+            
+            self.toggle_optimizer(optimizer_d)
+            
+            valid = torch.ones(imgs.size(0), 1) * (1 - self.hparams.label_smoothing_factor)
+            valid = valid.type_as(imgs)
+            
+            fake = torch.zeros(imgs.size(0), 1) + self.hparams.label_smoothing_factor
+            fake = fake.type_as(imgs)
 
-        real_loss = self.adversarial_loss(self.discriminator(imgs + discriminator_embedded_labels), valid)
+            real_loss = self.adversarial_loss(self.discriminator(imgs, embedded_labels), valid)
+            #real_loss = self.wasserstein_loss(self.discriminator(imgs, embedded_labels), valid)
 
-        fake = torch.zeros(imgs.size(0), 1)
-        fake = fake.type_as(imgs)
-
-        fake_loss = self.adversarial_loss(self.discriminator(self.generated_imgs.detach() + discriminator_embedded_labels), fake)
+            fake_loss = self.adversarial_loss(self.discriminator(self.generated_imgs.detach(), embedded_labels), fake)
+            
+            #d_loss = (real_loss + fake_loss) / 2
+            d_loss = real_loss + fake_loss
+            
+            self.manual_backward(d_loss, retain_graph=True)
+            if self.hparams.gradient_clipping_value > 0:
+                self.clip_gradients(optimizer_d, gradient_clip_val=self.gradient_clipping_value, gradient_clip_algorithm='norm')
+            optimizer_d.step()
+            optimizer_d.zero_grad()
         
-        d_loss = (real_loss + fake_loss) / 2
-        self.manual_backward(d_loss)
-        optimizer_d.step()
-        optimizer_d.zero_grad()
-        self.untoggle_optimizer(optimizer_d)
+            self.untoggle_optimizer(optimizer_d)
         
+        self.log('g_loss', g_loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log('d_loss', d_loss, on_step=True, on_epoch=True, prog_bar=True)
         
     def configure_optimizers(self):
-        lr = self.hparams.lr
+        g_lr = self.hparams.g_lr
+        d_lr = self.hparams.d_lr
         b1 = self.hparams.b1
         b2 = self.hparams.b2
         
-        opt_g = torch.optim.Adam(self.generator.parameters(), lr=lr, betas=(b1, b2))
-        opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=lr, betas=(b1, b2))
+        opt_g = torch.optim.Adam(self.generator.parameters(), lr=g_lr, betas=(b1, b2))
+        opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=d_lr, betas=(b1, b2))
+        #opt_d = torch.optim.RMSprop(self.discriminator.parameters(), lr=d_lr)
         
         return [opt_g, opt_d], []
 
@@ -151,19 +261,14 @@ class EmbeddedConditionlGAN(pl.LightningModule):
     
     def on_validation_epoch_end(self):
         if (self.current_epoch + 1) % 5 == 0:
-            generated_imgs = self(self.validation_z.to(self.device) + self.emb(self.validation_labels.to(self.device)))
+            
+            validation_z = torch.randn(10, self.latent_dim)
+            validation_labels = torch.arange(0, 10)
+            
+            generated_imgs = self(validation_z.to(self.device), self.emb(validation_labels.to(self.device)))
             grid = torchvision.utils.make_grid(generated_imgs, nrow=5)
             
             plt.figure(figsize=(10, 10))
             plt.imshow(grid.permute(1, 2, 0).cpu().numpy(), cmap='gray')
             plt.axis('off')
             plt.show()
-    
-    
-if __name__ == "__main__":
-    dm = MNISTDataModule()
-    dm.prepare_data()
-    dm.setup()
-    
-    gen = Generator(dm.dims)
-    gen(torch.randn(1, 100), Tensor([1]).long())
--- train.py
+++ train.py
@@ -1,10 +1,14 @@
 from model import EmbeddedConditionlGAN
 from datamodule import MNISTDataModule
 import pytorch_lightning as pl
+import torch
+
+torch.set_float32_matmul_precision("high")
 
 if __name__ == "__main__":
-    dm = MNISTDataModule(batch_size=256)
-    model = EmbeddedConditionlGAN(img_shape=dm.dims, lr=0.005, b1=0.5, b2=0.999)
-    trainer = pl.Trainer(max_epochs=500,accelerator="gpu")
+    dm = MNISTDataModule(batch_size=32)
+    model = EmbeddedConditionlGAN(img_shape=dm.dims, g_lr=0.0003, d_lr=0.0001, b1=0.5, b2=0.999, label_smoothing_factor=0, n_critics=5, gradient_clipping_value=0)
+    trainer = pl.Trainer(max_epochs=50,accelerator="gpu")
     
     trainer.fit(model, dm)
+    torch.save(model.state_dict(), "model.pth")
